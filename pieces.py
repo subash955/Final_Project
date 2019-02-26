17953865aee00d046cef8b9ffccbe52dd838a8ea
Subash Sundarraman <subash@Subashs-MacBook-Pro.local>
2019-02-26 02:10:33 -0800

    Added Parent class piece

--- pieces.py
+++ pieces.py
@@ -17,7 +17,22 @@ class Square:
 Board = [Square(x,y) for x in files for y in ranks]
 
 
+class piece:
+	
+	def __init__ (self, color):
+		self.status = True
+		self.value = 0
+		self.color = color
+		initial pos = Square()
 
+	def is_captured (self):
+		self.status = False
+
+	def move (self, new_pos):
+		self.position = new_pos
+        
+class pawn (piece):
+    
 
 
 	
