a24053a1c0c70095d90dfefbae8c1abf66ace769
Subash Sundarraman <subash@Subashs-MacBook-Pro.local>
2019-02-27 00:47:43 -0800

    Testing features

--- pieces.py
+++ pieces.py
@@ -1,38 +1,48 @@
-files = [A,B,C,D,E,F]
+files = [1: 'A', 2: 'B' , 3: 'C' ,4: 'D', 5: 'E', 6:'F', 7:'G', 8:'H']
 ranks = [1,2,3,4,5,6,7,8]
 
 class Square:
     def __init__(self, file, rank):
         self.rank = rank
         self.file = file
-        self.occupied = False
+        self.piece = 0
 
-    def changeoccupy (self):
-        self.occupied = !self.occupied
+    def setpiece (self, piece):
+    	self.piece = piece
     
     def __str__(self):
-        return (self.file + self.rank)
+        return (files[self.file] + self.rank)
 
         
-Board = [Square(x,y) for x in files for y in ranks]
+#Board = [Square(x,y) for x in files for y in ranks]
 
 
 class piece:
 	
 	def __init__ (self, color):
-		self.status = True
 		self.value = 0
 		self.color = color
-		initial pos = Square()
+		self.position = Square()
 
 	def is_captured (self):
 		self.status = False
 
 	def move (self, new_pos):
-		self.position = new_pos
+		position.setpiece(0)
+		position = new_pos
+		position.changeoccupy(self)	
         
 class pawn (piece):
     
+    def capture (self, new_pos):
+ 		if new_pos.rank == self.position.rank + 1 and abs(new_pos.file - self.position.file) == 1:
+ 			self.move(new_pos)
+ 		else:
+ 			print ("Error")
+ 		
+
+
+
 
 
 	
